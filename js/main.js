6366d250c19beda002edcf5af7e9171a3f3d7eef
sushantskb <117711778+Rahulgudu@users.noreply.github.com>
2023-02-03 01:56:16 +0530

    done the slider and added the movies section

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@
 
 // 28th code goes Headers, writing the script for swiper
 var swiper = new Swiper(".popular-content", {
-    spaceBetween: 30,
-    centeredSlides: true,
+    // 31st code goes Headers, adding slidesPerView
+    slidesPerView: 1,
+    spaceBetween: 10, /*-31st continues.. changing the value to 10 from 30-*/
+    // centeredSlides: true, /*31st contonues...changing removing this form code */
     autoplay: {
-        delay: 2500,
+        delay: 55500, /*31st continues...changing the value to 55500 from 2500 */
         disableOnIntearction: false,
     },
     pagination: {
@@ -16,4 +18,28 @@ var swiper = new Swiper(".popular-content", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+
+    // 32nd code goes Headers, adding the breakpoints 
+    breakpoints: {
+        280: {
+            slidesPerView: 1, 
+            spaceBetween: 10,
+        },
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        510: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        758: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        900: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    }
 });
