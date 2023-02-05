74096b080679f845b52883aca30e7b9159176f91
sushantskb <117711778+Rahulgudu@users.noreply.github.com>
2023-02-06 01:24:17 +0530

    Finished the website for dekstop view

--- js/main.js
+++ js/main.js
@@ -43,3 +43,22 @@ var swiper = new Swiper(".popular-content", {
         },
     }
 });
+
+
+// 67th code goes here, writing java script for play and close of video
+let playButton = document.querySelector(".play-movie");
+let video = document.querySelector(".video-container");
+let myvideo = document.querySelector("#myvideo");
+let closebtn = document.querySelector(".close-video");
+
+playButton.onclick = () => {
+    video.classList.add("show-video");
+    //Auto Play When Click on button
+    myvideo.play();
+
+};
+closebtn.onclick = () => {
+    video.classList.remove("show-video");
+    //Pause On Close Video
+    myvideo.pause();
+};
